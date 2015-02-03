3931e29411d34192c7e173c3de0c6efac71c2141
Brian Woodward <brian.woodward@gmail.com>
2015-02-03 11:25:47 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * accountdown-token <https://github.com/doowb/accountdown-token>
+ *
+ * Copyright (c) 2015 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
