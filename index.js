8765d61d9736dcaac3351a68754adecf92180792
Brian Woodward <brian.woodward@gmail.com>
2015-07-23 14:49:45 -0400

    running update to fix dot files and rebuild readme

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * accountdown-token <https://github.com/doowb/accountdown-token>
  *
- * Copyright (c) 2015 Brian Woodward, contributors.
- * Licensed under the MIT license.
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
  */
 
 'use strict';
